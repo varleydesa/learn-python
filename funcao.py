73a30d65be828e12ffc97de506a7e4ae9b88499a
varleydesa <varley.sa@ifma.edu.br>
2024-08-08 19:58:14 -0300

    funções

---
+++ funcao.py
@@ -0,0 +1,17 @@
+import math
+
+"""lado = input("Lado do Quadrado (m): ")
+
+def area_quadrado():
+    area = float(lado)**2
+    return area
+
+print("Área: "+str(area_quadrado())+" m²")"""
+
+raio = input("Raio do Círculo:")
+
+def area_circulo():
+    area = math.pi*float(raio)**2
+    return area
+
+print("Área: "+str(area_circulo())+" m²")
